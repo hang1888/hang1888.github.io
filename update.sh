ec98477ace383f33eb28969ec6f3fbf84bfe425d
挂 <geuan9678@gmail.com>
2026-08-13 16:04:57 +0800

    Move insulation to plugins section

--- update.sh
+++ update.sh
@@ -32,7 +32,8 @@ def section_for(block: str) -> str:
     if any(k in lower for k in [
         "com.lclrc.hammerit", "hammer it",
         "com.wkk.lookinloader", "lookinloader",
-        "com.be-huge.floating-view", "floatingview"
+        "com.be-huge.floating-view", "floatingview",
+        "com.be-huge.insulation", "insulation"
     ]):
         return "插件"
     # 微信插件
